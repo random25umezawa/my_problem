930e29d63d6386de396bb3496f1294e174689ae2
random25umezawa <bday7125@gmail.com>
2019-03-28 19:10:57 +0900

    矩形波圧縮ほぼ完成。

--- kukeiassyuku/main.cpp
+++ kukeiassyuku/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 using namespace std;
 
+typedef pair<int,int> P;
+
 int main() {
 	while(true) {
 		int n;
@@ -15,14 +17,20 @@ int main() {
 		for(int i = 0; i < n; i++) {
 			d_arr[i+1] = arr[i+1]-arr[i];
 		}
-		for(int i = 1; i <= n; i++) {
+		vector<P> ans;
+		for(int i = 1; i < n; i++) {
 			if(d_arr[i]>0) {
 				int d = d_arr[i];
-				cout << (i*2) << " " << d << endl;
+				P temp_ans(i*2,d);
+				ans.push_back(temp_ans);
 				for(int j = i; j <= n; j+=i) {
 					d_arr[j] += d*(((j/i)%2==0)?1:-1);
 				}
 			}
+		}
+		cout << ans.size() << endl;
+		for(int i = 0; i < ans.size(); i++) {
+			cout << ans[i].first << " " << ans[i].second << endl;
 		}
 	}
 	return 0;
--- kukeiassyuku/testcase.cpp
+++ kukeiassyuku/testcase.cpp
@@ -1,9 +1,34 @@
+#include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <random>
 using namespace std;
+
+mt19937 rnd;
+
+int get_rand(int a) {
+	if(a<=0) return 0;
+	return rnd() % a;
+}
+
+void set_combination(vector<int> &v, int a, int b, int amt) {
+	int len = b-a;
+	if(len<amt) return;
+	vector<int> cda(len);
+	for(int i = 1; i <= len; i++) cda[i-1] = i;
+	for(int i = 0; i < amt; i++) {
+		int rand_index = get_rand(cda.size());
+		v.push_back(cda[rand_index]);
+		//cout << cda[rand_index] << endl;
+		cda.erase(cda.begin()+rand_index);
+	}
+	sort(v.begin(),v.end());
+}
 
 void calc(int n, vector<int> &num, vector<int> &amt) {
 	if(num.size()!=amt.size()) return;
+
 	int len = num.size();
 	vector<int> imos(n+1);
 	for(int i = 0; i < len; i++) {
@@ -20,31 +45,53 @@ void calc(int n, vector<int> &num, vector<int> &amt) {
 		cout << " " << imos[i];
 	}
 	cout << endl;
+
+	/*
+	cout << num.size() << endl;
+	for(int i = 0; i < num.size(); i++) {
+		cout << (num[i]*2) << " " << amt[i] << endl;
+	}
+	*/
 }
 
 int main() {
-	{
-		vector<int> num{2,3,5,7,11};
-		vector<int> amt{1,1,1,1,1};
-		calc(25,num,amt);
+	int seed;
+	cin >> seed;
+	rnd = mt19937(seed);
+
+	for(int i = 0; i < 5; i++) {
+		vector<int> ls;
+		vector<int> ts;
+		int n = 2+get_rand(25);
+		int amt = 1+get_rand((n-2)/2);
+		set_combination(ls,1,n,amt);
+		for(int i = 0; i < amt; i++) {
+			ts.push_back(1+get_rand(5));
+		}
+		calc(n,ls,ts);
 	}
-	/*
-	{
-		vector<int> num(1000);
-		vector<int> amt(1000,1000);
-		for(int i = 1; i <= 1000; i++) num[i-1] = i;
-		calc(100000,num,amt);
+
+	for(int i = 0; i < 19; i++) {
+		vector<int> ls;
+		vector<int> ts;
+		int n = 1+get_rand(100000);
+		int amt = 1+get_rand((n-2)/2);
+		set_combination(ls,1,n,amt);
+		for(int i = 0; i < amt; i++) {
+			ts.push_back(1+get_rand(3125));
+		}
+		calc(n,ls,ts);
 	}
-	*/
+
 	{
-		vector<int> num{2,4,8,16,32};
-		vector<int> amt{2,4,8,16,32};
-		calc(1024,num,amt);
+		vector<int> num(50000);
+		vector<int> amt(50000);
+		for(int i = 0; i < 50000; i++) {
+			num[i] = ((seed&0b01)!=0)?i+1:50000-i;
+			amt[i] = ((seed&0b10)!=0)?i+1:50000-i;
+		}
+		calc(50000,num,amt);
 	}
-	{
-		vector<int> num{1,5,25,125,625,3125};
-		vector<int> amt{5,4,3,2,1,0};
-		calc(1024,num,amt);
-	}
+
 	cout << 0 << endl;
 }
